7891664c2d3b3680aae6c9c3b5c08e7bb116e048
alicevia <ed-phoenix@qq.com>
2023-01-17 17:48:19 +0800

    'feat:add-html-style'

---
+++ test/t.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div a="sdf" db="sfasdf">
+      <p>
+        sdf  
+      </p>
+      <p>sdf</p>
+      <p>sdf</p>
+      <p>sdf</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const a = {
+  a: 1,
+  b: 2,
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
